ee26749033d9506ed01bbe9e387ada95f92a9b48
TIAMUSE88 <baetinaj@gmail.com>
2026-03-23 21:43:44 +0000

    Input Functions and Typewriter scripts/ Form Handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Hold fast to dreams, for if dreams die",
+    autoStart: true,
+    delay: 1.5,
+    cursor: "",
+  });
+}
+
+let form = document.getElementById("poem-generator-form");
+form.addEventListener("submit", generatePoem);
